8bad961a9b1dfd1f9c3d3c4ad398bae0ef852768
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-15 18:49:51 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,26 @@
 #include "raylib.h"
+#include "config.hpp"
 #include "world.hpp"
 #include "player.hpp"
 
 int main() {
     InitWindow(0, 0, "Cubic Mobile");
-    
     World world;
     world.Generate();
-    
     Player player;
     player.Init();
-
     SetTargetFPS(60);
-
     while (!WindowShouldClose()) {
         player.Update();
-        
         BeginDrawing();
             ClearBackground(SKYBLUE);
             BeginMode3D(player.camera);
                 world.Draw();
-                DrawGrid(WORLD_SIZE, 1.0f);
             EndMode3D();
-            
             DrawCircle(GetScreenWidth()/2, GetScreenHeight()/2, 5, RED);
             DrawFPS(10, 10);
         EndDrawing();
     }
-    
     CloseWindow();
     return 0;
 }
